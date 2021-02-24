d10963a43c4d53de6b333bc678f2678a3be22453
uk <yachae85@gmail.com>
2021-02-24 18:11:37 +0900

    리액트 공부용 ch06-save

---
+++ ch06/save/jsx/savebutton.jsx
@@ -0,0 +1,10 @@
+class SaveButton extends React.Component {
+    handleSave(event) {
+        console.log(this, event)
+    }
+    render() {
+        return <button onClick={this.handleSave.bind(this)}>
+            save
+        </button>
+    }
+}
