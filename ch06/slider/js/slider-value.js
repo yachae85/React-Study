3e0045f3762eb8464b28d957ee967b7c4a3c544b
ungs <yachae85@gmail.com>
2021-02-24 20:53:08 +0900

    리액트 공부용 ch06-slider

---
+++ ch06/slider/js/slider-value.js
@@ -0,0 +1,24 @@
+class SliderValue extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSlide = this.handleSlide.bind(this);
+        this.state = { slideValue: 0 };
+    }
+    handleSlide(event) {
+        this.setState({ slideValue: event.detail.ui.value });
+    }
+    componentDidMount() {
+        window.addEventListener('slide', this.handleSlide);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('slide', this.handleSlide);
+    }
+    render() {
+        return React.createElement(
+            'div',
+            { className: '' },
+            'Value: ',
+            this.state.slideValue
+        );
+    }
+}
